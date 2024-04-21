e256a0182fd393fd1f010842fae06538332528ea
baesm971013 <bobbyb537@naver.com>
2024-04-21 20:04:04 +0900

    delete files

--- domain/question/question_crud.py
+++
@@ -1,6 +0,0 @@
-from models import Question
-from sqlalchemy.orm import Session
-
-def get_question_list(db: Session):
-    qlist = db.query(Question).order_by(Question.create_date.desc()).all()
-    return qlist
